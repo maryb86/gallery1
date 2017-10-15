d2a52b42c0425cd5cfc512af1a89f1d6cc2e0bc7
Maciejewski <mary_maciejewski@intuit.com>
2017-10-15 00:24:17 -0600

    Add image change by url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import update from 'immutability-helper';
 import Viewer from "./components/viewer";
 import Gallery from "./components/gallery"
@@ -27,8 +28,10 @@ class App extends Component {
       this.goBack = this.goBack.bind(this);
       this.goForward = this.goForward.bind(this);
       this.findImg = this.findImg.bind(this);
+      this.MyViewer = this.MyViewer.bind(this);
   }
 
+  //TODO: CAN THESE TWO BE COMBINED?
   findImg(e) {
       function lookForImg(element) {
           return e.target.src.includes(element.fileName);
@@ -37,10 +40,46 @@ class App extends Component {
       return imageIndex;
   }
 
-  //TODO: CHANGE GALLERY HIGHLIGHT ON BACK AND FORWARD (REACT LIFE CYCLE)
-  swapViewerImg(e) {
+  findImgByFileName(fileName) {
+      function lookForImg(element) {
+          return element.fileName.includes(fileName);
+      }
+      const imageIndex = this.state.images.findIndex(lookForImg);
+      return imageIndex;
+  }
+
+  //TODO: LOSE THE DUPLICATION OF SWAPVIEWERIMG
+  swapViewerImg(e, imageInfo) {
       const previousImage = this.state.viewerImg;
-      const chosenImage = this.findImg(e);
+      let chosenImage = "";
+      //TODO: CREATE LOCAL VAR FOR IMAGEINFO PROPERTIES FOR BETTER READING
+      if (imageInfo && (imageInfo.imageIndex || imageInfo.imageIndex === 0)) {
+          chosenImage = imageInfo.imageIndex;
+      } else {
+          if (imageInfo && imageInfo.fileName) {
+              chosenImage = this.findImgByFileName(imageInfo.fileName);
+          } else {
+              chosenImage = this.findImg(e);
+          }
+      }
+      if (this.state.viewerImg !== chosenImage) {
+          const images = update(this.state.images, {
+              [chosenImage]: {selected: {$set: true}}
+          });
+          delete images[previousImage].selected;
+          this.setState({
+              viewerImg: chosenImage,
+              images: images
+          });
+      }
+  }
+
+  swapViewerImgByUrl(imageInfo) {
+      const previousImage = this.state.viewerImg;
+      let chosenImage = this.state.viewerImg;
+      if (imageInfo && imageInfo.fileName) {
+          chosenImage = this.findImgByFileName(imageInfo.fileName);
+      };
       if (this.state.viewerImg !== chosenImage) {
           const images = update(this.state.images, {
               [chosenImage]: {selected: {$set: true}}
@@ -54,41 +93,69 @@ class App extends Component {
   }
 
   goBack(e) {
-      const prevImg = this.state.viewerImg - 1
+      const prevImg = this.state.viewerImg - 1;
       if (prevImg >= 0){
-          this.setState({
-              viewerImg: prevImg
-          })
+          this.swapViewerImg(e, {
+              imageIndex: prevImg
+          });
       } else {
-          this.setState({
-              viewerImg: this.state.images.length - 1
-          })
+          this.swapViewerImg(e, {
+              imageIndex: this.state.images.length - 1
+          });
       }
   }
 
   goForward(e) {
       const nextImg = this.state.viewerImg + 1
       if (nextImg <= this.state.images.length - 1 ){
-          this.setState({
-              viewerImg: nextImg
-          })
+          this.swapViewerImg(e, {
+              imageIndex: nextImg
+          });
       } else {
-          this.setState({
-              viewerImg: 0
-          })
+          this.swapViewerImg(e, {
+              imageIndex: 0
+          });
       }
-   }
+  }
+
+  //TODO: RENAME THIS TO SOMETHING BETTER
+  MyViewer(props) {
+      let viewerImgName = "";
+      //TODO: P0 - FIX BUG! DEFAULT NOT SHOWING WHEN NO IMAGE PROVIDED IN URL
+      //TODO: P1 - FIX BUG! CHANGE URL WHEN IMAGE CLICKED - SEE ERROR IN CONSOLE
+      //TODO: TEST OUT DIFFERENT URLS THAT ARE INVALID
+      if (props.match.params.image) {
+          this.swapViewerImgByUrl({
+              fileName: props.match.params.image
+          });
+      }
+
+      return (
+          <div>
+            <Viewer
+                viewerImg={this.state.viewerImg}
+                images={this.state.images}
+                goBack={this.goBack}
+                goForward={this.goForward}
+                //{...props}
+            />
+          </div>
+      );
+  }
 
   render() {
     return (
       <div className="App">
         <header className="app-header">
           <h1 className="app-title">Mary's collection of homemade toys</h1>
-          <Viewer
-              viewerImg={this.state.viewerImg}
-              images={this.state.images}
-              goBack={this.goBack}
-              goForward={this.goForward}
+          <Route
+              path="/:image"
+              render={this.MyViewer}
+          />
+          <Route
+              exact
+              path="/"
+              render={() => (<Redirect to={`/${this.state.images[0].fileName}`}/>)}
           />
         </header>
         <Gallery
